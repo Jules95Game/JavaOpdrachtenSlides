42a98cabdf87ba77257cd5ebf9cf7c3189787ab9
Jules95Game <romp.julian1@gmail.com>
2024-01-25 15:58:34 +0100

    H1 - testing static variables

--- h1_eindopdracht/classes/Message.java
+++ h1_eindopdracht/classes/Message.java
@@ -1,5 +1,6 @@
 package classes;
 
-public class Message{
-    public String testMessage = "Test1";
+public class Message {
+  public String testMessage1 = "Test 1";
+  public static String testMessage2 = "Test 2";
 }
--- h1_eindopdracht/entrypoint/Program.java
+++ h1_eindopdracht/entrypoint/Program.java
@@ -4,9 +4,15 @@ package entrypoint;
 // I want to use the class Message.java
 import classes.Message;
 
-public class Program{
-    public static void main(String[] args){
-        Message test = new Message();
-        System.out.println(test.testMessage);
-    }
+public class Program {
+  public static void main(String[] args) {
+    Message testObject1 = new Message();
+    System.out.println(testObject1.testMessage1);
+
+    // testMessage2 is a static variable and can be used without a Message object
+    System.out.println(Message.testMessage2);
+
+    testObject1.testMessage1 = "Test 3";
+    System.out.println(testObject1.testMessage1);
+  }
 }
