d9f5a29347bd32dfc8229fc79d2e2fac854a4df4
Jules95Game <romp.julian1@gmail.com>
2024-01-25 13:36:33 +0100

    Initial files

---
+++ java_traineeship/com/cert/Opdracht_1.java
@@ -0,0 +1,7 @@
+package com.cert;
+
+public class Opdracht_1 {
+public static void main(String[] args){
+System.out.println(7);
+}
+}
---
+++ packageTest1/universiteit/Examen.java
@@ -0,0 +1,9 @@
+package universiteit;
+import certificering.Examenvraag;
+
+public class Examen{
+    static Examenvraag ev;
+    public static void main(String[] args){
+        System.out.println(ev.vraag);
+}
+}
