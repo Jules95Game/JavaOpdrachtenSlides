465eefab2ad2e90541f867f0b6f8552574ca4dfb
Jules95Game <romp.julian1@gmail.com>
2024-02-06 14:45:58 +0100

    Update

--- h1_eindopdracht/entrypoint/Program.java
+++ h1_eindopdracht/entrypoint/Program.java
@@ -7,12 +7,25 @@ import classes.Message;
 public class Program {
   public static void main(String[] args) {
     Message testObject1 = new Message();
+    Message testObject2 = new Message();
     System.out.println(testObject1.testMessage1);
+    System.out.println(testObject2.testMessage1);
+    System.out.println();
 
     // testMessage2 is a static variable and can be used without a Message object
     System.out.println(Message.testMessage2);
+    System.out.println();
 
+    // testMessage1 is tied to testObject1 and has no effect on other Message objects
     testObject1.testMessage1 = "Test 3";
     System.out.println(testObject1.testMessage1);
+    System.out.println(testObject2.testMessage1);
+    System.out.println();
+
+    // testMessage2 is shared among all Message objects
+    Message.testMessage2 = "Test 4";
+    System.out.println(testObject1.testMessage2);
+    System.out.println(testObject2.testMessage2);
+    System.out.println(Message.testMessage2);
   }
 }
